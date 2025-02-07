09fb80d56e4d445239f2a7aec0dc485667b4249f
Mr.Lau <lxy060505@gmail.com>
2025-02-07 13:39:31 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,29 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'MrLau Status',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur2825424-54e761c6d2a6331d07633955',
+  ],
+
+  // 日志天数
+  CountDays: 60,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Index',
+      url: 'https://lxyss.com/'
+    },
+    {
+      text: 'Blog',
+      url: 'https://blog.lxyss.com/'
+    },
+  ],
+};
